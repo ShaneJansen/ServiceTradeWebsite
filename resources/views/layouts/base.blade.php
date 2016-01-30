4076f1b1b474558dc8f1b65d88859da1e65096e4
Shane Jansen <shanejjansen@gmail.com>
2016-01-29 21:27:59 -0500

    Fixed strange issue with footer overlap.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,7 @@
         <title>ServiceTrade - Improve your skills and get work done.</title>
         @yield('base.headMeta')
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.0.0/angular-material.min.css">
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.0.4/angular-material.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="/dist/css/base.css">
@@ -24,7 +24,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-route.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-resource.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-cookies.min.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/angular_material/1.0.0/angular-material.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/angular_material/1.0.4/angular-material.min.js"></script>
         @yield('base.bodyJavaScript')
     </body>
 </html>
--- resources/views/layouts/unauthed/main.blade.php
+++ resources/views/layouts/unauthed/main.blade.php
@@ -4,9 +4,9 @@
     <div ng-app="unauthedApp" ng-cloak layout="column" layout-fill>
         <div ng-controller="MainController as main" layout="column" layout-fill>
             <md-content layout="column" flex md-scroll-y>
-                <div layout="column"> @yield('main.body')</div>
-                <span layout="column" flex></span>
-                <div layout="column"> @include('layouts.footer') </div>
+                <div> @yield('main.body')</div>
+                <span flex></span>
+                <div> @include('layouts.footer') </div>
             </md-content>
             @section('base.bodyJavaScript')
                 <script src="/dist/js/unauthed.js"></script>
